9858ef20030289bd624d1c6853c858b996927ef0
Vedant Deshpande <55455919+Vedant0605@users.noreply.github.com>
2020-04-30 23:17:29 +0530

    loading and cleaning data

---
+++ newspredict.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+from scipy.stats import skew
+
+
+df = pd.read_csv('news.csv')
+print(df.columns)
+
+
+
+
